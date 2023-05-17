89e82848197be43ed41617dcd8335a241bc90260
Frostork <simon.walenczak@lacatholille.fr>
2023-05-17 16:36:12 +0200

    setup

---
+++ Assets/Scripts/TileBoard.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBoard : MonoBehaviour
+{
+    public Tile tilePrefab;
+    private TileGrid grid;
+    private List<Tile> tiles;
+
+    private void Awake()
+    {
+        grid = GetComponentInChildren<TileGrid>();
+        tiles = new List<Tile>(16);
+    }
+
+    private void Start()
+    {
+        CreateTile();
+        CreateTile();
+    }
+
+    private void CreateTile()
+    {
+        Tile tile = Instantiate(tilePrefab, grid.transform);
+    }
+}
