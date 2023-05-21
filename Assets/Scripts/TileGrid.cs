aa6baa463ed1ab259a03b2f8368ed15e97baec33
Frostork <92367148+SimonWalenczak@users.noreply.github.com>
2023-05-22 01:10:43 +0200

    Tout

--- Assets/Scripts/TileGrid.cs
+++ Assets/Scripts/TileGrid.cs
@@ -1,5 +1,5 @@
-using System;
 using UnityEngine;
+using Random = System.Random;
 
 public class TileGrid : MonoBehaviour
 {
@@ -25,5 +25,56 @@ public class TileGrid : MonoBehaviour
                 rows[y].cells[x].coordinates = new Vector2Int(x, y);
             }
         }
+    }
+
+    public TileCell GetCell(int x, int y)
+    {
+        if (x >= 0 && x < width && y >= 0 && y < height)
+        {
+            return rows[y].cells[x];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public TileCell GetCell(Vector2Int coordinates)
+    {
+        return GetCell(coordinates.x, coordinates.y);
+    }
+    
+    public TileCell GetAdjacentCell(TileCell cell, Vector2Int direction)
+    {
+        Vector2Int coordinates = cell.coordinates;
+
+        coordinates.x += direction.x;
+        coordinates.y -= direction.y;
+
+        return GetCell(coordinates);
+    }
+    
+    public TileCell GetRandomEmptyCell()
+    {
+        Random r = new Random();
+        int index = r.Next(0, cells.Length);
+        int startingIndex = index;
+
+        while (cells[index].occupied)
+        {
+            index++;
+
+            if (index >= cells.Length)
+            {
+                index = 0;
+            }
+
+            if (index == startingIndex)
+            {
+                return null;
+            }
+        }
+
+        return cells[index];
     }
 }
